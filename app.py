6d9656b044cf53edb48c42702f52aef22a1cd034
Robert Wang <robwang.us@gmail.com>
2024-02-02 13:26:11 -0600

    basic formatting of the website

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("mainpage.html")
 
