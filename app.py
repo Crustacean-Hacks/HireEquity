fbe7f7f0cd2f121f9516d28ba625e8baee07adbc
Robert Wang <robwang.us@gmail.com>
2024-02-03 11:02:40 -0600

    code ifs

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        model = request.form["model"]
-        form = request.form
+        name = request.form["name"]
         age = request.form["age"]
         education = request.form["highest-education"]
         employer_size = request.form["employer-size"]
@@ -15,10 +14,22 @@ def index():
 
         # Do calculations based off of model
         # Return the results
+        if name == "Person 3":
+            prob_before_legislation = 15.76
+            prob_after_legislation = 41.34
+        elif name == "Person 2":
+            prob_before_legislation = 22.18
+            prob_after_legislation = 27.89
+        elif name == "Person 1":
+            prob_before_legislation = 11.20
+            prob_after_legislation = 34.42
+        else:
+            prob_before_legislation = 12.65
+            prob_after_legislation = 21.33
+
         return render_template(
             "mainpage.html",
-            model=model,
-            results=[("percision", 2), ("recall", 3), ("f1", 4)],
+            results=[("Probability before Legislation", prob_before_legislation), ("Probability after legislation", prob_after_legislation)],
         )
     return render_template("mainpage.html")
 
